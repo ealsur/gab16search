f1fc9d224a2751825c25fbd61ef2c5bf9fe8d945
Matías Quaranta <ealsur@hotmail.com>
2016-03-15 22:08:42 -0300

    basic search service

--- Services/SearchService.cs
+++ Services/SearchService.cs
@@ -1,11 +1,13 @@
 
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Azure.Search;
 using Microsoft.Azure.Search.Models;
 using Newtonsoft.Json;
 
 public interface ISearchService
 {
-    string Search(string searchText,SearchParameters sp);
+    string Search(SearchPayload payload);
 }
 
 public class SearchService:ISearchService
@@ -16,7 +18,21 @@ public class SearchService:ISearchService
         client = new SearchServiceClient(searchAccount, new SearchCredentials(searchKey));     
         indexClient = client.Indexes.GetClient("movies");   
     }    
-    public string Search(string searchText, SearchParameters sp){
-        return JsonConvert.SerializeObject(indexClient.Documents.Search(searchText, sp));
-    }        
+    public string Search(SearchPayload payload){
+        var sp = new SearchParameters();
+        sp.Top = payload.PageSize;
+        sp.Skip = (payload.Page - 1) * payload.PageSize;
+        sp.Filter = string.Join(",", payload.Filters.Select(x=>string.Format("{0}={1}", x.Key, x.Value)).ToArray());
+        sp.OrderBy = payload.OrderBy.Split(',');
+        sp.QueryType = payload.QueryType;
+        sp.SearchMode = payload.SearchMode;
+        if(!string.IsNullOrEmpty(payload.ScoringProfile)){
+            sp.ScoringProfile = payload.ScoringProfile;    
+        }        
+        sp.IncludeTotalResultCount = true;
+        if(payload.IncludeFacets){
+            sp.Facets = new List<string>(){"year", "rtAllCriticsRating", "actorTags", "genreTags"};
+        }
+        return JsonConvert.SerializeObject(indexClient.Documents.Search(payload.Text, sp));
+    }     
 }
