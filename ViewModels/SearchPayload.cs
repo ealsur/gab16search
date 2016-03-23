1ec7debb0d2ce580c537fe776b136cb97575dee4
Matías Quaranta <ealsur@hotmail.com>
2016-03-23 00:44:56 -0300

    smaller pages

--- ViewModels/SearchPayload.cs
+++ ViewModels/SearchPayload.cs
@@ -6,7 +6,7 @@ namespace gab16search.ViewModels
     public class SearchPayload
     {
         public int Page { get; set; }=1;
-        public int PageSize { get; set; } = 25;
+        public int PageSize { get; set; } = 10;
         
         public bool IncludeFacets { get; set; } = false;
         public string Text { get; set; }
