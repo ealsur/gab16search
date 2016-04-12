dd3bf025aef3e9c2452cd2a778fabcf5ac8720f2
Matías Quaranta <ealsur@hotmail.com>
2016-04-11 22:32:11 -0300

    fix scoring

--- Services/SearchService.cs
+++ Services/SearchService.cs
@@ -42,14 +42,15 @@ public class SearchService:ISearchService
             sp.HighlightFields = new List<string>(){"text"};
         }
         if(!string.IsNullOrEmpty(payload.ScoringProfile)){
-            sp.ScoringProfile = payload.ScoringProfile;    
+            sp.ScoringProfile = payload.ScoringProfile;
+            if(!string.IsNullOrEmpty(payload.ScoringParameter)){
+                sp.ScoringParameters = payload.ScoringParameter.Split(',').Select(x=> new ScoringParameter("likes", payload.ScoringParameter)).ToList();    
+            }
+            else{
+                if(payload.ScoringParameter=="")
+                sp.ScoringParameters = new List<ScoringParameter>(){ new ScoringParameter("likes", "none")};
+            }    
         } 
-         if(!string.IsNullOrEmpty(payload.ScoringParameter)){
-            sp.ScoringParameters = payload.ScoringParameter.Split(',').Select(x=> new ScoringParameter("likes", payload.ScoringParameter)).ToList();    
-        }          
-        else if(!string.IsNullOrEmpty(payload.ScoringProfile)){
-            sp.ScoringParameters = payload.ScoringParameter.Split(',').Select(x=> new ScoringParameter("likes", "none")).ToList();
-        }
         sp.IncludeTotalResultCount = true;
         if(payload.IncludeFacets){
             /* FACETS FOR APPLIED FILTERS ARE NOT REQUIRED */
